347146e86a2230b91fb6bcc1fb3b435b3d125534
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2020-01-20 13:27:46 +0000

    build(backend): setting up backend and docker initial setup

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
